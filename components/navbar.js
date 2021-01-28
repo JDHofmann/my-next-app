024e98f5077415dcd68eab4c8ea6455e2442b2df
jdhofmann <justindhofmann@gmail.com>
2021-01-27 21:01:22 -0500

    css module 2

--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,12 @@
 import Link from 'next/link'
+import style from './navbar.module.css'
 
 export default function Navbar(){
     return(
-        <header>
-            <Link href="/"><a>Home</a></Link>
-            <Link href="/posts"><a>Posts</a></Link>
-            <Link href="/about"><a>About</a></Link>
+        <header className={style.header}>
+            <Link href="/"><a className={style.navlink}>Home</a></Link>
+            <Link href="/posts"><a className={style.navlink}>Posts</a></Link>
+            <Link href="/about"><a className={style.navlink}>About</a></Link>
         </header>
     )
 }
