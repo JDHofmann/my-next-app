4dc0128edc1ffc366f37e5ccb2ffc13d57c9484e
jdhofmann <justindhofmann@gmail.com>
2021-02-09 10:42:25 -0500

    organized scss

--- components/navbar.js
+++ components/navbar.js
@@ -1,17 +1,12 @@
 import Link from 'next/link'
-import style from './navbar.module.css'
+// import style from './navbar.module.css'
 
 export default function Navbar(){
     return(
-        <header className={style.header}>
-            <Link href="/"><a className={style.navlink}>Home</a></Link>
-            <Link href="/posts"><a className={style.navlink}>Posts</a></Link>
-            <Link href="/about"><a className={style.navlink}>About</a></Link>
-            {/* <style jsx>{`
-                header {
-                    background: #d0d0d0;
-                }
-            `}</style> */}
+        <header className='header'>
+            <Link href="/"><a className='navlink'>Home</a></Link>
+            <Link href="/posts"><a className='navlink'>Posts</a></Link>
+            <Link href="/about"><a className='navlink'>About</a></Link>
         </header>
     )
 }
